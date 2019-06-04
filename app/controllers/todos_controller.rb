fc0b7378f0b2a120912ec36d155c13cc17bba0d2
oscariogithub <oscar.r.arancibia@gmail.com>
2019-06-04 12:03:58 -0400

    Parte II Ok

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,56 @@
 class TodosController < ApplicationController
+
+    def index
+        @todos = Todo.all
+    end
+
+
+    def new
+        @todo = Todo.new
+    end
+
+
+
+    def create
+        @todo = Todo.new(todo_params)
+        @todo.completed = false
+        @todo.save
+        redirect_to todos_path   
+    end
+
+
+
+    def show
+        @todo = Todo.find(params[:id])
+    end
+
+
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+
+
+
+    def update
+        @todo = Todo.find(params[:id])
+        @todo.update(todo_params)
+        redirect_to todos_path
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.delete
+        redirect_to todos_path
+    end
+
+
+
+    private
+
+    def todo_params
+        params.require(:todo).permit(:description, :completed)
+    end
+
+
+
 end
